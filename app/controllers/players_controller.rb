1a1d2a78af654c17c07cbf26e3a29e0b8afdcd4b
lucastimho <lucas.t.ho.lh@gmail.com>
2021-11-19 13:56:21 -0600

    adds create and update method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,7 +9,28 @@ class PlayersController < ApplicationController
   def create
     response = HTTP.get("https://api.opendota.com/api/players/#{params[:account_id]}").parse(:json)
     player = Player.new(
-      
+      account_id: params[:account_id],
+      name: response["profile"]["name"],
+      person_name: response["profile"]["personname"],
+      mmr: response["mmr_estimate"]["estimate"]
     )
+    if player.save
+      render json: player
+    else 
+      render json: {errors: player.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+  def update
+    response = HTTP.get("https://api.opendota.com/api/players/#{params[:account_id]}").parse(:json)
+    player = Player.find_by(id: params[:id])
+    player.account_id = params[:account_id]
+    player.name = response["profile"]["name"]
+    player.person_name = response["profile"]["personname"]
+    player.mmr = response["mmr_estimate"]["estimate"]
+    if player.save
+      render json: player
+    else
+      render json: {errors: player.errors.full_messages}, status: 418
+    end
   end
 end
